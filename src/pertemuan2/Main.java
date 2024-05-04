4fbed0db5aa47eb8909bc1f8e90009d24141f38c
danieltampubolon29 <tampubolondaniel29@gmail.com>
2024-05-04 13:32:27 +0700

    first commit

--- src/pertemuan2/Main.java
+++ src/pertemuan2/Main.java
@@ -1,105 +1,53 @@
 package pertemuan2;
-
-abstract class Animal {
+class Animal{
     private String animal;
     private String name;
     private String gender;
-
-    // Constructor
-    Animal(String animal, String name, String gender) {
+    // Contructor
+    Animal(String animal, String name, String gender){
         this.animal = animal;
         this.name = name;
         this.gender = gender;
-    }
-
-    // Getter and setter
-    public String getAnimal() {
-        return animal;
-    }
-
-    public void setAnimal(String animal) {
-        this.animal = animal;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    // Display common animal data
-    void display() {
-        System.out.println("Type of Animal \t: " + animal + "\nAnimal name \t: " + name + "\nAnimal gender \t: " + gender);
-        System.out.println(getWeightInfo());
-        getVoiceInfo();
-    }
-
-    // Abstract methods to be implemented by subclasses
-    abstract String getWeightInfo();
-    abstract void getVoiceInfo();
-}
-
-class Detail extends Animal {
-    private int weight;
-    private String voice;
-
-    Detail(String animal, String name, String gender, int weight, String voice) {
-        super(animal, name, gender);
-        this.weight = weight;
-        this.voice = voice;
     }
     // Getter and Setter
-    public int getWeight() {
-        return weight;
+    public String getAnimal(){
+        return animal;
+    }
+    public void setAnimal(String animal){
+        this.animal = animal;
+    }
+    public String getName(){
+        return animal;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public void setGender(String gender){
+        this.gender = gender;
     }
 
-    public void setWeight(int weight) {
-        this.weight = weight;
+    public void display(){
+        System.out.println("Type of Animal \t: " + animal + "\nAnimal name : " + name + "\nAnimal gender \t: " + gender + "\n");
     }
 
-    public String getVoice() {
-        return voice;
-    }
-
-    public void setVoice(String voice) {
-        this.voice = voice;
-    }
-
-    @Override
-    String getWeightInfo() {
-        return ("Animal weight \t: " + weight + " kg");
-    }
-
-    @Override
-    void getVoiceInfo() {
-        System.out.println( "Animal voice \t: " + voice + "\n");
-    }
 }
-
-
 public class Main {
-public static void main(String[] args) {    
-    // make object animal
-    Detail animal1 = new Detail("Dog", "Kyno", "male", 6, "guk guk");
-    Detail animal2 = new Detail("Cat", "Katy", "female", 2, "meow");
+public static void main(String[] args) {
+    // membuat object
+    Animal animal1 = new Animal("Dog", "Kyno", "Male");
+    Animal animal2 = new Animal("Cat", "Cassie", "Female");
     // animal 1
     animal1.display();
-
+    // change the value of setter
+    animal1.setName("Blackie");
+    animal1.display();
     // animal 2
     animal2.display();
-    // change value of set
-    animal2.setName("Cassie");
-    animal2.setWeight(3);
+    // change the value of setter
+    animal2.setAnimal("Bird");
     animal2.display();
 }
 }
