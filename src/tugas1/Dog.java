dd965f8f4e293cd89870281211eb0bf9dbea777a
danieltampubolon29 <tampubolondaniel29@gmail.com>
2024-05-11 13:42:00 +0700

    Inside inheritance class you must write code with operator and garbage collector inside

---
+++ src/tugas1/Dog.java
@@ -0,0 +1,15 @@
+package tugas1;
+
+public class Dog extends Animal {
+    private int weight;
+
+    Dog(String name, String gender, int weight) {
+        super(name, gender);
+        this.weight = weight;
+    }
+
+    @Override
+    public void detail() {
+        System.out.println("Dog weight \t: " + this.weight + "\n" + getName() + " Say Guk Guk!");
+    }
+}
